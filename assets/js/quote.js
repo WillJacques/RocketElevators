742e6d8bc5137cb18a22d3994e1ca4996f9db298
WillJacques <68091327+WillJacques@users.noreply.github.com>
2020-09-28 07:55:03 -0400

    ProblemMerge

--- assets/js/quote.js
+++ assets/js/quote.js
@@ -1,12 +1,16 @@
-/*
+
 $(document).ready(function () {
     $('#selectMe').change(function () {
         $('.building_type').hide();
         $('#' + $(this).val()).show();
     })
 });
-*/
-$("#selectMe, input[type=radio][name=exampleRadios], #mcalc-napartsr, #mcalc-nfloorsr, #mcalc-nbasementsr, #mcalc-nfloorscm, #mcalc-nbasementscm, #mcalc-nbusinesscm, #mcalc-nparkingcm, #mcalc-nelevatorcm, #mcalc-nparkingcr, #mcalc-ntenantcr, #mcalc-noccupantcr, #mcalc-nfloorscr, #mcalc-nbasementscr, #mcalc-nbusinessh, #mcalc-nfloorsh, #mcalc-nbasementsh, #mcalc-nparkingh, #mcalc-noccupanth, #mcalc-nhoursh").change(function () {
+
+$("input[type=radio][name=exampleRadios]").change(function () {
+    mcalc();
+});
+
+$("#selectMe, #mcalc-napartsr, #mcalc-nfloorsr, #mcalc-nbasementsr, #mcalc-nfloorscm, #mcalc-nbasementscm, #mcalc-nbusinesscm, #mcalc-nparkingcm, #mcalc-nelevatorcm, #mcalc-nparkingcr, #mcalc-ntenantcr, #mcalc-noccupantcr, #mcalc-nfloorscr, #mcalc-nbasementscr, #mcalc-nbusinessh, #mcalc-nfloorsh, #mcalc-nbasementsh, #mcalc-nparkingh, #mcalc-noccupanth, #mcalc-nhoursh").keyup(function () {
     mcalc();
 });
 
@@ -34,6 +38,7 @@ function mcalc() {
     if (selectedtype === "") {
         var result_columns = 0;
         var result_elevators = 0;
+
         $("#mcalc-result-elevators").text(result_elevators);
         $("#mcalc-result-columns").text(result_columns);
 
@@ -46,6 +51,7 @@ function mcalc() {
         var result_elevators = 1;
         result_columns = Math.ceil(ntotalfloors / 20);
         result_elevators = Math.ceil(mcalc_naparts / mcalc_nfloors / 6) * result_columns;
+
         $("#mcalc-result-elevators").text(result_elevators);
         $("#mcalc-result-columns").text(result_columns);
 
@@ -59,6 +65,7 @@ function mcalc() {
         var result_elevators = 1;
         result_elevators = mcalc_nelevator;
         result_columns = result_elevators;
+
         $("#mcalc-result-elevators").text(result_elevators);
         $("#mcalc-result-columns").text(result_columns);
 
